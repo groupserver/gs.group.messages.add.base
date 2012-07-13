31171156d737c08f4625e824a108082d4cec91b7
Michael JasonSmith <mpj17@onlinegroups.net>
2012-07-13 12:23:51 +1000

    Rewroking everything to get it into the right directory.
    --HG--
    rename : gs/group/messages/__init__.py => gs/group/messages/add/__init__.py
    rename : gs/group/messages/add/__init__.py => gs/group/messages/add/base/__init__.py
    rename : gs/group/messages/add/addapost.py => gs/group/messages/add/base/addapost.py
    rename : gs/group/messages/add/addemail.py => gs/group/messages/add/base/addemail.py
    rename : gs/group/messages/add/adder.py => gs/group/messages/add/base/adder.py
    rename : gs/group/messages/add/base.py => gs/group/messages/add/base/base.py
    rename : gs/group/messages/add/browser/templates/addemail.pt => gs/group/messages/add/base/browser/templates/addemail.pt
    rename : gs/group/messages/add/browser/templates/groupexists.pt => gs/group/messages/add/base/browser/templates/groupexists.pt
    rename : gs/group/messages/add/configure.zcml => gs/group/messages/add/base/configure.zcml
    rename : gs/group/messages/add/groupexists.py => gs/group/messages/add/base/groupexists.py
    rename : gs/group/messages/add/interfaces.py => gs/group/messages/add/base/interfaces.py
    rename : gs/group/messages/add/smtp2gs.py => gs/group/messages/add/base/smtp2gs.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from version import get_version
 
 version = get_version()
 
-setup(name='gs.group.messages.add',
+setup(name='gs.group.messages.add.base',
     version=version,
-    description="Add a message to a GroupServer group",
+    description="The core code for ading a message to a GroupServer group",
     long_description=open("README.txt").read() + "\n" +
                       open(os.path.join("docs", "HISTORY.txt")).read(),
     classifiers=[
@@ -27,20 +27,16 @@ setup(name='gs.group.messages.add',
     url='http://groupserver.org/',
     license='other',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
+    namespace_packages=['gs', 'gs.group', 'gs.group.messages', 
+                        'gs.group.messages.add'],
     include_package_data=True,
     zip_safe=True,
     install_requires=[
         'setuptools',
-        'lockfile',
         'gs.auth.token',
         'Products.XWFMailingListManager', # For the mailing list
         # -*- Extra requirements: -*-
     ],
     entry_points={
-        'console_scripts': [
-            'smtp2gs = gs.group.messages.add.smtp2gs:main',
-            ],
-
     },
 )
