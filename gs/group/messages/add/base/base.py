39968a985d70623d0c1d8962416dc17938dc89c2
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-12 14:16:55 -0700

    Updating an import

--- gs/group/messages/add/base/base.py
+++ gs/group/messages/add/base/base.py
@@ -13,7 +13,7 @@
 #
 ##############################################################################
 from zope.cachedescriptors.property import Lazy
-from gs.content.form import SiteForm
+from gs.content.form.base import SiteForm
 
 
 class ListInfoForm(SiteForm):
