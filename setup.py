9b5eacb89ce4f1c24bd9d4046b8f18731c2c63a4
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-13 16:18:06 -0700

    Upating the product deps.

--- setup.py
+++ setup.py
@@ -12,17 +12,22 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='gs.group.messages.add.base',
     version=version,
     description="The core code for ading a message to a GroupServer group",
-    long_description=open("README.txt").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
+    long_description=long_description,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -30,14 +35,14 @@ setup(name='gs.group.messages.add.base',
         "Intended Audience :: Developers",
         'License :: OSI Approved :: Zope Public License',
         "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
       ],
-    keywords='groupserver message post topic images',
+    keywords='groupserver, message, post, email, list',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='http://groupserver.org/',
+    url='https://source.iopen.net/groupserver/gs.group.member.add.base/',
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.group', 'gs.group.messages',
@@ -46,10 +51,21 @@ setup(name='gs.group.messages.add.base',
     zip_safe=False,
     install_requires=[
         'setuptools',
+        'pytz',
+        'zope.browserpage',
         'zope.cachedescriptors',
         'zope.component',
+        'zope.formlib',
         'zope.interface',
+        'zope.schema',
+        'zope.tal',
+        'zope.tales',
+        'Zope2',
         'gs.auth.token',
+        'gs.content.form.base',
+        'gs.email',
+        'gs.group.member.canpost',
+        'gs.profile.notify',
         'Products.GSAuditTrail',
         'Products.XWFMailingListManager',  # For the mailing list
         'Products.XWFCore',
