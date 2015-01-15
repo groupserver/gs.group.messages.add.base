800689ebefd5aa129e5345e84f7a1f49c2a06a82
Michael JasonSmith <mpj17@onlinegroups.net>
2015-01-14 17:14:29 -0800

    Pointing setup.py at GitHub

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
-# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# Copyright © 2012, 2014, 2015 OnlineGroups.net and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -11,7 +11,7 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
 import codecs
 import os
 from setuptools import setup, find_packages
@@ -21,10 +21,12 @@ version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
-with codecs.open(os.path.join("docs", "HISTORY.rst"), encoding='utf-8') as f:
+with codecs.open(os.path.join("docs", "HISTORY.rst"),
+                 encoding='utf-8') as f:
     long_description += '\n' + f.read()
 
-setup(name='gs.group.messages.add.base',
+setup(
+    name='gs.group.messages.add.base',
     version=version,
     description="The core code for ading a message to a GroupServer group",
     long_description=long_description,
@@ -37,12 +39,11 @@ setup(name='gs.group.messages.add.base',
         "Natural Language :: English",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
+        "Topic :: Software Development :: Libraries :: Python Modules", ],
     keywords='groupserver, message, post, email, list',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='https://source.iopen.net/groupserver/gs.group.member.add.base/',
+    url='https://github.com/groupserver/gs.group.member.add.base/',
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.group', 'gs.group.messages',
